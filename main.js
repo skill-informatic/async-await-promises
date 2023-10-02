ba9d7d989b56d45097940671476d2baf088549ab
skill-informatic <mariojhaziel04@gmail.com>
2023-10-01 23:59:39 -0600

    mainIndexStyle

---
+++ main.js
@@ -0,0 +1,71 @@
+const elements = document.querySelector("#elements");
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+  const result = await getUsersPromises(url);
+  console.log("result", result);
+  showContent(result);
+});
+async function getUsersAwait(url) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(
+        `Error al obtener los usuarios. Código de estado: ${response.status}`
+      );
+    }
+
+    const users = await response.json();
+    showContent(users);
+    console.log("Usuarios await:");
+    console.log(users);
+    return users;
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+function getUsersPromises(url) {
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          `Error al obtener los usuarios. Código de estado: ${response.status}`
+        );
+      }
+      return response.json();
+    })
+    .then((users) => {
+      showContent(users);
+      console.log("Usuarios Promises:");
+      console.log(users);
+      return users;
+    })
+    .catch((error) => {
+      console.error(`Error: ${error.message}`);
+    });
+}
+
+const showContent = (users) => {
+  // Utilizamos un fragmento para construir los elementos antes de agregarlos al DOM
+  const fragment = document.createDocumentFragment();
+
+  // Recorremos la lista de usuarios y creamos los elementos
+  users.forEach((user) => {
+    const userElement = document.createElement("div");
+    userElement.id = user.id;
+    userElement.innerHTML = `
+        <h3>Name: ${user.name}</h3>
+        <p>Email: ${user.email}</p>
+      `;
+    fragment.appendChild(userElement);
+  });
+
+  // Limpiamos el contenido existente en elements
+  elements.innerHTML = "";
+
+  // Agregamos los elementos al DOM de una sola vez
+  elements.appendChild(fragment);
+};
