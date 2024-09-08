d4b1a2f2591e0123023ba778b8a1f3db40c9ddd4
skill Informatic <130352904+skill-informatic@users.noreply.github.com>
2024-09-08 03:16:46 -0600

    alert

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const elements = document.querySelector("#elements");
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const url = "https://jsonplaceholder.typicode.com/11";
+  const url = "https://jsonplaceholder.typicode.com/users";
 
   const result = await getUsersAwait(url);
-  console.log("result", result?result:"Hubo un error");
+  console.log("result", result);
+
   showContent(result);
 });
 async function getUsersAwait(url) {
@@ -23,7 +24,7 @@ async function getUsersAwait(url) {
     console.log(users);
     return users;
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    return `Error: ${error.message}`;
   }
 }
 
@@ -44,31 +45,37 @@ function getUsersPromises(url) {
       return users;
     })
     .catch((error) => {
-      console.error(`Error: ${error.message}`);
+      return `Error: ${error.message}`;
     });
 }
 
 const showContent = (users) => {
   // Utilizamos un fragmento para construir los elementos antes de agregarlos al DOM
   const fragment = document.createDocumentFragment();
-
   // Recorremos la lista de usuarios y creamos los elementos
-  if (users) {
-    users.forEach((user) => {
-      const userElement = document.createElement("div");
-      userElement.id = user.id;
-      userElement.innerHTML = `
+  console.log(users);
+  if (typeof users === "string") {
+    elements.innerHTML = "";
+    const Alert = document.createElement("h3");
+    Alert.textContent = "Hubo un Error";
+    Alert.classList.add("alert");
+    return elements.appendChild(Alert);
+  }
+
+  users.forEach((user) => {
+    const userElement = document.createElement("div");
+    userElement.id = user.id;
+    userElement.innerHTML = `
           <h3>Name: ${user.name}</h3>
           <p>Email: ${user.email}</p>
         `;
-      fragment.appendChild(userElement);
-    });
-  
-    // Limpiamos el contenido existente en elements
-    elements.innerHTML = "";
-  
-    // Agregamos los elementos al DOM de una sola vez
-    elements.appendChild(fragment);
-  }
-  
+    fragment.appendChild(userElement);
+  });
+
+  // Limpiamos el contenido existente en elements
+  elements.innerHTML = "";
+
+  // Agregamos los elementos al DOM de una sola vez
+  return elements.appendChild(fragment);
 };
+
